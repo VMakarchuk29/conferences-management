fe56004204ddada01d113189984d188788aeea9e
VMakarchuk29 <vm29v07@gmail.com>
2021-04-15 13:56:12 +0300

    Create login and register page with localization

---
+++ src/main/java/com/conference/demo/config/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.conference.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/webjars/**", "/images/**", "/signup**", "/login").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
+    }
+}
---
+++ src/main/java/com/conference/demo/controller/AuthController.java
@@ -0,0 +1,18 @@
+package com.conference.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String registrationForm() {
+        return "registration";
+    }
+}
